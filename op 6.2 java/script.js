8fc052390c2b71aecd2247e73e6d28b719334852
JustinKooledu <9019785@student.zadkine.nl>
2022-10-12 13:32:07 +0200

    6.2

---
+++ op 6.2 java/script.js
@@ -0,0 +1,10 @@
+function changecolor (){
+    alert("het werkt");
+    console.log("het werkt in de console");
+}
+
+function calculator(num, num2) {
+    let answer = num + num2;
+    console.log(answer)
+    alert("het antwoord is 23")
+}
