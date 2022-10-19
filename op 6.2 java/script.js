0a1642a6b0d3a7f6fe7b2242a7fa8897ea4028cf
JustinKooledu <9019785@student.zadkine.nl>
2022-10-19 21:00:29 +0200

    6.4

--- op 6.2 java/script.js
+++ op 6.2 java/script.js
@@ -3,13 +3,25 @@ function changecolor (){
     console.log("het werkt in de console");
 }
 
+function changetext (){
+    document.getElementById("1"). textContent = "hallo";
+}
+
 function calculator(num, num2) {
+    let text = prompt("ask your question")
     let answer = num + num2;
     console.log(answer)
 }
 
 function multiplybyfive (num) {
+    let text = prompt("wich number needs to be multiplied?")
     let answer = num * 5;
     console.log(answer)
-    alert("35")
+    alert(answer)
+}
+function sectomin (num) {
+    let text = prompt("how many seconds need to be converted to minutes?")
+    let answer = num / 60;
+    console.log(answer)
+    alert(answer)
 }
