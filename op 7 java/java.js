9dcb1a549e4f4a9f923a395d06b8b78cf8366f47
JustinKooledu <9019785@student.zadkine.nl>
2022-10-20 10:30:37 +0200

    7.2

---
+++ op 7 java/java.js
@@ -0,0 +1,19 @@
+function budget() {
+    let budget = 100
+    let product = Number(prompt("het kost het product?"))
+    if (budget > product) {
+        alert("U heeft genoeg geld!")
+    }
+    else {
+        alert("Helaas, te weinig geld")
+    }
+}
+
+function clicker () {
+    if (Num > 10) {
+        set num to 0
+    }
+    else (num + 1) {
+        
+    }
+}
