56074dfed736ced83a9c4397d43e6a0873917075
JustinKooledu <9019785@student.zadkine.nl>
2022-10-20 08:51:09 +0200

    6.4

--- op 6.2 java/script.js
+++ op 6.2 java/script.js
@@ -7,20 +7,21 @@ function changetext (){
     document.getElementById("1"). textContent = "hallo";
 }
 
-function calculator(num, num2) {
-    let text = prompt("ask your question")
+function calculator() {
+    let num = Number(prompt("give your first number"))
+    let num2 = Number(prompt("give your second number"))
     let answer = num + num2;
     console.log(answer)
 }
 
-function multiplybyfive (num) {
-    let text = prompt("wich number needs to be multiplied?")
+function multiplybyfive () {
+    let num = Number(prompt("wich number needs to be multiplied?"))
     let answer = num * 5;
     console.log(answer)
     alert(answer)
 }
-function sectomin (num) {
-    let text = prompt("how many seconds need to be converted to minutes?")
+function sectomin () {
+    let num = Number(prompt("how many seconds need to be converted to minutes?"))
     let answer = num / 60;
     console.log(answer)
     alert(answer)
